b9efc9dcba8e352f49b3a0c06d603f6e9b8614c9
plantstoen <plantstoen@gmail.com>
2020-08-04 20:46:37 +0900

    add: 404.tsx

---
+++ pages/404.tsx
@@ -0,0 +1,10 @@
+import Head from 'next/head'
+import Link from 'next/link'
+
+export default function Forbidden() {
+  return (
+    <div className="container">
+      <span>존재하지 않는 페이지입니다!</span>
+    </div>
+  )
+}
---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+import Link from 'next/link'
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <span>반갑습니다</span>
+    </div>
+  )
+}
