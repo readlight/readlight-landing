f0f6a813f5102737286d018617250bd4cf7f7590
plantstoen <plantstoen@gmail.com>
2020-08-06 08:35:35 +0900

    refactor: remove non-used div  in NavTextButton

--- components/atom/NavTextButton.tsx
+++ components/atom/NavTextButton.tsx
@@ -4,19 +4,12 @@ import Link from 'next/link';
 
 type Props = { buttonText: string; targetPath: string };
 
-const ButtonClickArea = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 60px;
-`;
-
 const NavText = styled.span`
   color: ${({ theme }) => theme.colors.pureBlack};
   font-size: 16px;
   font-weight: normal;
-  padding: 5px 0px;
-  margin: 0px 15px;
+  padding: 10px 6px;
+  margin: 0px 20px;
 
   &:hover {
     cursor: pointer;
@@ -25,11 +18,9 @@ const NavText = styled.span`
 
 const NavTextButton: React.FC<Props> = ({ buttonText, targetPath }) => {
   return (
-    <ButtonClickArea>
-      <Link href={targetPath}>
-        <NavText>{buttonText}</NavText>
-      </Link>
-    </ButtonClickArea>
+    <Link href={targetPath}>
+      <NavText>{buttonText}</NavText>
+    </Link>
   );
 };
 
