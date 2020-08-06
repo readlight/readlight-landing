f72b345a2104f00d2f459050b6dcc226c56fb52b
plantstoen <plantstoen@gmail.com>
2020-08-06 18:39:34 +0900

    add: background-transition scode in NavbarWrapper

--- components/template/NavbarWrapper.tsx
+++ components/template/NavbarWrapper.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import styled from 'styled-components';
-import NavButtonList from '../molecule/NavButtonList';
-import LogoButton from '../atom/LogoButton';
+import useScrollEvent from '../../utils/useScrollEvent';
 
 type Props = { children: React.ReactNode };
 
@@ -11,13 +10,15 @@ const NavbarBox = styled.div`
   top: 0;
   right: 0;
   left: 0;
-  background-color: ${({ theme }) => theme.colors.pureWhite};
+  background-color: ${(props) =>
+    props.scrollPosition > 2 ? 'white' : 'transparent'};
   transition: background-color 0.25s ease-in-out;
   --webkit-transform: background-color 0.25s ease-in-out;
 `;
 
 const NavbarWrapper: React.FC<Props> = ({ children }) => {
-  return <NavbarBox>{children}</NavbarBox>;
+  const { scrollPosition, scrollDirection } = useScrollEvent();
+  return <NavbarBox scrollPosition={scrollPosition}>{children}</NavbarBox>;
 };
 
 export default NavbarWrapper;
