caf5aa34ac308892112ff9649ffcd73f0e9cf4fa
plantstoen <plantstoen@gmail.com>
2020-08-07 09:28:51 +0900

    add: useFeatureList hook with initializing, change Data based on Enum

--- components/atom/FeatureTextButton.tsx
+++ components/atom/FeatureTextButton.tsx
@@ -3,8 +3,13 @@ import styled from 'styled-components';
 import Link from 'next/link';
 import FeatureContext from '../../utils/feature.context';
 import { FeatureList } from '../../types/FeatureList';
+import useFeatureList from '../../utils/useFeatureList';
 
-type Props = { buttonText: string; isSelected: boolean };
+type Props = {
+  buttonText: string;
+  isSelected: boolean;
+  featureType: FeatureList;
+};
 
 const NavText = styled.span`
   color: ${({ theme }) => theme.colors.pureBlack};
@@ -15,16 +20,26 @@ const NavText = styled.span`
   border-bottom: ${(props) =>
     props.isSelected ? '2px solid #3B4048' : '0px solid'};
   transition: color 230ms ease-in-out;
+  user-select: none;
 
   &:hover {
     cursor: pointer;
   }
 `;
 
-const FeatureTextButton: React.FC<Props> = ({ buttonText, isSelected }) => {
-  //const { changeFeature } = useContext(FeatureContext);
+const FeatureTextButton: React.FC<Props> = ({
+  buttonText,
+  isSelected,
+  featureType,
+}) => {
+  const { changeFeature } = useFeatureList();
   return (
-    <NavText onClick={() => {}} isSelected={isSelected}>
+    <NavText
+      onClick={() => {
+        changeFeature(featureType);
+      }}
+      isSelected={isSelected}
+    >
       {buttonText}
     </NavText>
   );
--- components/molecule/FeatureChange.tsx
+++ components/molecule/FeatureChange.tsx
@@ -1,7 +1,9 @@
 import React, { useContext, useState, useEffect } from 'react';
 import styled from 'styled-components';
 import FeatureTextButton from '../atom/FeatureTextButton';
-import FeatureContext from '../../utils/feature.context';
+//import FeatureContext from '../../utils/feature.context';
+import useFeature from '../../utils/useFeatureList';
+import { FeatureList } from '../../types/FeatureList';
 
 const FeatureChangeWrapper = styled.div`
   margin-top: 90px;
@@ -9,11 +11,24 @@ const FeatureChangeWrapper = styled.div`
 `;
 
 const NavButtonList = () => {
+  const { featureBooleanList } = useFeature();
   return (
     <FeatureChangeWrapper>
-      <FeatureTextButton buttonText={'오늘의 책'} isSelected={false} />
-      <FeatureTextButton buttonText={'컬렉션'} isSelected={true} />
-      <FeatureTextButton buttonText={'리더스픽'} isSelected={false} />
+      <FeatureTextButton
+        buttonText={'오늘의 책'}
+        isSelected={featureBooleanList[0]}
+        featureType={FeatureList.BOOKOFTODAY}
+      />
+      <FeatureTextButton
+        buttonText={'컬렉션'}
+        isSelected={featureBooleanList[1]}
+        featureType={FeatureList.COLLECTION}
+      />
+      <FeatureTextButton
+        buttonText={'리더스픽'}
+        isSelected={featureBooleanList[2]}
+        featureType={FeatureList.READERSPICK}
+      />
     </FeatureChangeWrapper>
   );
 };
