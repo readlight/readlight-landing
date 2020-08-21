7d2c8c14e14a3491a18794d30b3b70b893511c8c
Taeyeon <ruadl034@naver.com>
2020-08-22 00:46:26 +0900

    refactor: ActiveSmallText.tsx

--- components/atom/ActiveSmallText.tsx
+++ components/atom/ActiveSmallText.tsx
@@ -1,16 +1,20 @@
 import * as React from 'react';
 import styled from 'styled-components';
 
-type Props = { children: React.ReactNode; isActive: boolean };
+interface IProps {
+  children: React.ReactNode;
+  isActive: boolean;
+}
 
 const InlineText = styled.span`
-  color: ${(props) => (props.isActive ? 'black' : '#C6C6C6')};
+  color: ${(props: IProps) => (props.isActive ? 'black' : '#C6C6C6')};
   transition: color 230ms ease-in-out;
 
   @media (max-width: 768px) {
   }
 `;
-const ActiveSmallText: React.FC<Props> = ({ children, isActive }) => {
+
+const ActiveSmallText = ({ children, isActive }: IProps) => {
   return <InlineText isActive={isActive}>{children}</InlineText>;
 };
 
