45813fb5c00040f6a517ae462fad978fc19a9d61
Taeyeon <ruadl034@naver.com>
2020-08-22 13:04:10 +0900

    refactor: FeatureTextButton.tsx

--- components/atom/FeatureTextButton.tsx
+++ components/atom/FeatureTextButton.tsx
@@ -9,12 +9,12 @@ interface IProps {
   changeFeature: Function;
 }
 
-interface NavText {
+interface INavText {
   isSelected: boolean;
 }
 
 const NavText = styled.span`
-  color: ${(props: NavText) => (props.isSelected ? '#0E62FF' : '#C6C6C6')};
+  color: ${(props: INavText) => (props.isSelected ? '#0E62FF' : '#C6C6C6')};
   font-size: 20px;
   font-weight: ${(props) => (props.isSelected ? 'bold' : 'bold')};
   border-bottom: ${(props) =>
