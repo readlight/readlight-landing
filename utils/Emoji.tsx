bc0c92580fce4dc861260a1d547f3266168f1d2a
plantstoen <plantstoen@gmail.com>
2020-08-23 16:26:24 +0900

    fix: Emoji.tsx overload error

--- utils/Emoji.tsx
+++ utils/Emoji.tsx
@@ -1,19 +1,26 @@
 import React from 'react';
 import styled from 'styled-components';
 
-type Props = {
+interface IProps {
   padding: string;
   emojiSize: string;
   symbol: any;
   label: string;
-};
+}
 
-const EmojiWrapper = styled.span`
+// styled-components의 Props용
+interface StyledProps {
+  padding: string;
+  emojiSize: string;
+}
+
+// 계속 오버로드를 찾을 수 없다해서 다음과 같이 수정
+const EmojiWrapper = styled.span<StyledProps>`
   padding: ${(props) => props.padding};
   font-size: ${(props) => props.emojiSize};
 `;
 
-const Emoji = ({ padding, emojiSize, symbol, label }: Props) => (
+const Emoji = ({ padding, emojiSize, symbol, label }: IProps) => (
   <EmojiWrapper
     className="emoji"
     role="img"
