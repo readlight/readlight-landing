807ab92f0d78ee87791b326cb0fa8b5e04c0221e
Taeyeon <ruadl034@naver.com>
2020-08-25 08:44:15 +0900

    refactor: remove ChangeableText.tsx

--- components/atom/SmallText.tsx
+++ components/atom/SmallText.tsx
@@ -9,10 +9,24 @@ const InlineText = styled.span`
   color: ${({ theme }) => theme.colors.pureBlack};
 
   @media (max-width: 768px) {
+    text-align: center;
   }
 `;
+
+const TextWrapper = styled.div`
+  @media (max-width: 768px) {
+    width: 80%;
+    margin-left: auto;
+    margin-right: auto;
+  }
+`;
+
 const SmallText = ({ children }: IProps) => {
-  return <InlineText>{children}</InlineText>;
+  return (
+    <InlineText>
+      <TextWrapper>{children}</TextWrapper>
+    </InlineText>
+  );
 };
 
 export default SmallText;
--- components/template/DailyFeed.tsx
+++ components/template/DailyFeed.tsx
@@ -2,9 +2,9 @@ import * as React from 'react';
 import ContentHeader from '../molecule/ContentHeader';
 import FeatureChange from '../molecule/FeatureChange';
 import styled from 'styled-components';
-import ChangeableText from '../atom/ChangeableText';
 import useFeature from '../../utils/useFeature';
 import MockupDeviceWrapper from '../organism/MockupWrapper';
+import SmallText from '../atom/SmallText';
 
 interface IProps {
   isMobile: boolean;
@@ -30,9 +30,12 @@ const ContentArea = styled.div`
 `;
 
 const textData = {
-  'bookoftoday' : '하루 한권, 인공지능이 당신의 독서 취향을 분석해 책을 추천해줍니다.',
-  'collection' : '리드라이트 큐레이터들이 최근의 이슈나 트렌드에 맞는 책들을 엄선해 제공합니다.',
-  'readerspick' : '독서 결정에 도움을 줄수 있도록 큐레이터가 책을 읽고 작성한 칼럼을 제공합니다.',
+  bookoftoday:
+    '하루 한권, 인공지능이 당신의 독서 취향을 분석해 책을 추천해줍니다.',
+  collection:
+    '리드라이트 큐레이터들이 최근의 이슈나 트렌드에 맞는 책들을 엄선해 제공합니다.',
+  readerspick:
+    '독서 결정에 도움을 줄수 있도록 큐레이터가 책을 읽고 작성한 칼럼을 제공합니다.',
 };
 
 const DailyFeed = ({ isMobile }: IProps) => {
@@ -66,7 +69,7 @@ const DailyFeed = ({ isMobile }: IProps) => {
           featureBooleanList={featureBooleanList}
           changeFeature={changeFeature}
         />
-        <ChangeableText textData={textData[feature]} />
+        <SmallText>{textData[feature]}</SmallText>
       </ContentArea>
     </DaliyFeedWrapper>
   );
